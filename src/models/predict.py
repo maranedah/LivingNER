5bd3bb8d33aa0196087924a473339a4cd00d5c26
Jose Barros <jose.barros.s@ug.uchile.cl>
2022-06-11 08:52:10 -0400

    Final tweaks

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,26 +1,22 @@
+from html import entities
 from pathlib import Path
 from typing import Dict, List, Tuple
 from flair.models.sequence_tagger_model import SequenceTagger
 from flair.data import Sentence, Token, Span
+import numpy as np
 import pandas as pd
 
-from src.utils import Split, DATA_PATH, MODELS_PATH, RESULTS_PATH, read_sentences
+from src.utils import DOCUMENTS_PATH, Split, DATA_PATH, MODELS_PATH, RESULTS_PATH, Task, read_sentences
 from src.models.string_distance_matcher import StringMatcher
 
 
-def predict_subtask1():
+def predict_subtask1(split_to_predict=Split.test_background):
     model_filepath = MODELS_PATH / "species.pt"
     prediction_filepath = RESULTS_PATH / "species_predictions.tsv"
-    split_to_predict = Split.valid
     predict_flair_model(model_filepath, prediction_filepath, split_to_predict)
 
 
-def predict_subtask2(split_to_predict=Split.valid, read_nosocomial_predictions=False):
-    if not read_nosocomial_predictions:
-        predict_flair_model(
-            MODELS_PATH / "nosocomial.pt", RESULTS_PATH / "nosocomial_predictions.tsv", split_to_predict
-        )
-
+def predict_subtask2():
     code_matcher_filepath = MODELS_PATH / "codes_string_matcher.pickle"
     sm = StringMatcher.load(code_matcher_filepath)
     species_predictions = pd.read_csv(RESULTS_PATH / "species_predictions.tsv", sep="\t")
@@ -33,16 +29,6 @@ def predict_subtask2(split_to_predict=Split.valid, read_nosocomial_predictions=F
     only_humans_df["code"] = ["9606"] * only_humans_df.shape[0]
     predictions_df = pd.concat([only_humans_df, only_species_df])
 
-    nosocomial_df = pd.read_csv(RESULTS_PATH / "nosocomial_predictions.tsv", sep="\t")
-    nosocomial_df = nosocomial_df[["filename", "off0", "off1", "label"]]
-
-    predictions_df = predictions_df.merge(
-        nosocomial_df, on=["filename", "off0", "off1"], how="left", suffixes=(None, "_nosocomial")
-    )
-
-    predictions_df["isN"] = predictions_df["label_nosocomial"] == "NOSOCOMIAL"
-    predictions_df["isH"] = predictions_df["code"].str.contains("H")
-    predictions_df["iscomplex"] = predictions_df["code"].str.contains("\|")
     predictions_df["NCBITax"] = predictions_df["code"]
 
     predictions_columns = ["filename", "mark", "label", "off0", "off1", "span", "NCBITax"]
@@ -50,13 +36,63 @@ def predict_subtask2(split_to_predict=Split.valid, read_nosocomial_predictions=F
     predictions_df.to_csv(RESULTS_PATH / "subtask2_predictions.tsv", sep="\t", index=False)
 
 
+def predict_subtask3(read_entities=False, split_to_predict=Split.test_background):
+    if not read_entities:
+        predict_all_entities(split_to_predict=split_to_predict)
+    subtask2_predictions = pd.read_csv(RESULTS_PATH / "subtask2_predictions.tsv", sep="\t")
+    entities_dataframes = []
+    map_entity2 = {"pet": "Pet", "animal": "AnimalInjury", "food": "Food", "nosocomial": "Nosocomial"}
+    for entity_rc in ["pet", "animal", "food", "nosocomial"]:
+        entity_df = pd.read_csv(RESULTS_PATH / f"{entity_rc}_predictions.tsv", sep="\t")
+        entity_df = entity_df[["filename", "off0", "off1", "label"]]
+
+        entities_df = subtask2_predictions.merge(
+            entity_df, on=["filename", "off0", "off1"], suffixes=(None, f"_{entity_rc}"), how="inner"
+        )
+        entities_df = entities_df.groupby("filename", as_index=False).agg({f"NCBITax": "+".join})
+        entities_df[f"is{map_entity2[entity_rc]}"] = [True] * entities_df.shape[0]
+        entities_df[f"{map_entity2[entity_rc]}IDs"] = entities_df["NCBITax"]
+        entities_df = entities_df[["filename", f"is{map_entity2[entity_rc]}", f"{map_entity2[entity_rc]}IDs"]]
+        entities_dataframes.append(entities_df)
+
+    filenames = read_filenames(split_to_predict, Task.subtask3)
+    predictions_df = pd.DataFrame(data={"filename": filenames})
+    for df in entities_dataframes:
+        predictions_df = predictions_df.merge(df, on=["filename"], how="left")
+
+    for entity in map_entity2.values():
+        predictions_df[f"is{entity}"] = predictions_df[f"is{entity}"].replace(True, "Yes")
+        predictions_df[f"is{entity}"] = predictions_df[f"is{entity}"].replace(np.nan, "No")
+        predictions_df[f"{entity}IDs"] = predictions_df[f"{entity}IDs"].replace(np.nan, "NA")
+
+    predictions_df.to_csv(RESULTS_PATH / "subtask3_predictions.tsv", sep="\t", index=False)
+
+
+def read_filenames(split_to_predict: Split, subtask: Task):
+    if split_to_predict == Split.valid and subtask == Task.subtask3:
+        with open(
+            DOCUMENTS_PATH / split_to_predict.value / Task.subtask3.value / "validation_files_task3.txt", "r"
+        ) as f:
+            return f.read().splitlines()
+    else:
+        return [file for file in (DOCUMENTS_PATH / split_to_predict.value / "text-files").iterdir()]
+
+
+def predict_all_entities(split_to_predict=Split.valid):
+    for entity in ["food"]:
+        model_filepath = MODELS_PATH / f"{entity}.pt"
+        prediction_filepath = RESULTS_PATH / f"{entity}_predictions.tsv"
+        predict_flair_model(model_filepath, prediction_filepath, split_to_predict)
+
+
 def predict_flair_model(model_filepath: Path, prediction_filepath: Path, split_to_predict: Split):
     print("Loading model")
+    print(model_filepath)
     trained_model: SequenceTagger = SequenceTagger.load(model_filepath)
     print("Loaded model")
     sentences = read_sentences(split_to_predict)
     print("Read sentences")
-    trained_model.predict([s for s, o in sentences], label_name="predicted_ner")
+    trained_model.predict([s for s, o in sentences], label_name="predicted_ner", verbose=True)
     rows = []
     for sentence, original_text in sentences:
         labels_predicted = transform_to_format(sentence, original_text)
